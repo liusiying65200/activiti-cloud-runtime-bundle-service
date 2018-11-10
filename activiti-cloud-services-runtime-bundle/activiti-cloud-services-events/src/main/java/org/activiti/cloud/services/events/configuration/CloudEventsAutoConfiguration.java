658002675641e0fa5141763cf96a4c62222fbb77
Igor Dianov <igor@dianov.com>
2018-11-10 15:54:44 -0800

    chore: Polish MessageBuilderFilterChainFactory to apply generic payload

--- activiti-cloud-services-runtime-bundle/activiti-cloud-services-events/src/main/java/org/activiti/cloud/services/events/configuration/CloudEventsAutoConfiguration.java
+++ activiti-cloud-services-runtime-bundle/activiti-cloud-services-events/src/main/java/org/activiti/cloud/services/events/configuration/CloudEventsAutoConfiguration.java
@@ -47,7 +47,7 @@ import org.activiti.cloud.services.events.listeners.CloudVariableDeletedProducer
 import org.activiti.cloud.services.events.listeners.CloudVariableUpdatedProducer;
 import org.activiti.cloud.services.events.listeners.MessageProducerCommandContextCloseListener;
 import org.activiti.cloud.services.events.listeners.ProcessEngineEventsAggregator;
-import org.activiti.cloud.services.events.message.CloudRuntimeEventsMessageBuilderFilterChainFactory;
+import org.activiti.cloud.services.events.message.CommandContextMessageBuilderFilterChainFactory;
 import org.activiti.cloud.services.events.message.MessageBuilderFilterChainFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -64,8 +64,8 @@ public class CloudEventsAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean(MessageBuilderFilterChainFactory.class)
-    public CloudRuntimeEventsMessageBuilderFilterChainFactory cloudRuntimeEventsMessageBuilderFilterChainFactory(RuntimeBundleProperties properties) {
-        return new CloudRuntimeEventsMessageBuilderFilterChainFactory(properties);
+    public CommandContextMessageBuilderFilterChainFactory commandContextMessageBuilderFilterChainFactory(RuntimeBundleProperties properties) {
+        return new CommandContextMessageBuilderFilterChainFactory(properties);
     }
 
     @Bean
@@ -83,9 +83,9 @@ public class CloudEventsAutoConfiguration {
     @Bean
     @ConditionalOnMissingBean
     public MessageProducerCommandContextCloseListener apiMessageProducerCommandContextCloseListener(ProcessEngineChannels processEngineChannels,
-                                                                                                    CloudRuntimeEventsMessageBuilderFilterChainFactory runtimeBundleMessageBuilderFilterChainFactory) {
+                                                                                                    CommandContextMessageBuilderFilterChainFactory commandContextMessageBuilderFilterChainFactory) {
         return new MessageProducerCommandContextCloseListener(processEngineChannels,
-                                                              runtimeBundleMessageBuilderFilterChainFactory);
+                                                              commandContextMessageBuilderFilterChainFactory);
     }
 
     @Bean
